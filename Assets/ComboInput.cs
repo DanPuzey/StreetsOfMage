e7c9b42bf1778d30c29921d3046a49388453adc6
Dan Puzey <commit@puzey.net>
2016-01-29 22:39:44 +0000

    Basic combo matcher with gamepad button bindings (axes not working).

---
+++ Assets/ComboInput.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using UnityEngine;
+
+namespace WizardDuel
+{
+    public class ComboInput : MonoBehaviour
+    {
+        #region Inspector fields
+        public string ButtonPrefix = "joystick 1 ";
+        public string AxisPrefix = "Joy 1 ";
+        public string[] InputButtons =
+        {
+            "button 0",
+            "button 1",
+            "button 2",
+            "button 3",
+            null,
+            null,
+            null,
+            null
+        };
+        public AxisInfo[] InputAxes =
+        {
+            null,
+            null,
+            null,
+            null,
+            new AxisInfo("Axis 7", true),
+            new AxisInfo("Axis 7", false),
+            new AxisInfo("Axis 6", false),
+            new AxisInfo("Axis 6", true)
+        };
+        public float AxisDeadzone = 0.2f;
+
+        public ComboMatcher[] Targets = new ComboMatcher[0];
+
+        private int _controlCount;
+        private string[] _buttons;
+        private AxisInfo[] _axes;
+        #endregion
+
+        private void Awake()
+        {
+            _controlCount = InputButtons.Length;
+            _buttons = new string[_controlCount];
+            _axes = new AxisInfo[_controlCount];
+            
+            for (var i = 0; i < _controlCount; i++)
+            {
+                if (!string.IsNullOrEmpty(InputButtons[i]))
+                {
+                    _buttons[i] = string.Concat(ButtonPrefix, InputButtons[i]);
+                }
+
+                if (InputAxes[i] != null && !string.IsNullOrEmpty(InputAxes[i].Name))
+                {
+                    var axisName = string.Concat(AxisPrefix, InputAxes[i].Name);
+                    _axes[i] = new AxisInfo(axisName, InputAxes[i].Positive);
+                }
+            }
+        }
+
+        private void Update()
+        {
+            for (var i = 0; i < InputButtons.Length; i++)
+            {
+                if (_buttons[i] != null && Input.GetKeyDown(_buttons[i]))
+                {
+                    SendGlyph(i);
+                    return;
+                }
+
+                AxisInfo axis = _axes[i];
+
+                if (axis != null)
+                {
+                    var axisValue = Input.GetAxis(axis.Name);
+
+                    if (Mathf.Abs(axisValue) > AxisDeadzone)
+                    {
+                        if (axis.Positive && axisValue > 0)
+                        {
+                            SendGlyph(i);
+                            return;
+                        }
+                        else if (!axis.Positive && axisValue < 0)
+                        {
+                            SendGlyph(i);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void SendGlyph(int index)
+        {
+            Debug.Log("Sending glyph");
+
+            foreach (var c in Targets)
+            {
+                c.AddGlyph(index);
+            }
+        }
+
+        [Serializable]
+        public class AxisInfo
+        {
+            public string Name;
+            public bool Positive;
+
+            public AxisInfo(string name, bool positive)
+            {
+                Name = name;
+                Positive = positive;
+            }
+        }
+    }
+}
---
+++ Assets/ComboMatcher.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+
+namespace WizardDuel
+{
+    public class ComboMatcher : MonoBehaviour
+    {
+        [Header("Combo setup")]
+        public int AlphabetSize = 8;
+        public string[] Alphabet = { "A", "B", "X", "Y", "up", "down", "left", "right" };
+
+        public int[] Combo = { 1, 2, 3, 4 };
+
+        [Header("Behaviour")]
+        public bool ResetOnMiss = true;
+
+        private int _currentSymbolIndex = 0;
+
+        public void AddGlyph(int markIndex)
+        {
+            if (markIndex == _currentSymbolIndex)
+            {
+                _currentSymbolIndex++;
+                
+                if (_currentSymbolIndex >= Combo.Length)
+                {
+                    CompleteCombo();
+                }
+            }
+            else
+            {
+                if (ResetOnMiss)
+                {
+                    Reset();
+                }
+            }
+        }
+
+        public void Reset()
+        {
+            _currentSymbolIndex = 0;
+            Debug.Log("Combo reset");
+        }
+
+        private void CompleteCombo()
+        {
+            Debug.LogFormat("Combo complete!", Combo);
+
+            if (ComboCompleted != null)
+            {
+                ComboCompleted(this, EventArgs.Empty);
+            }
+
+            _currentSymbolIndex = 0;
+        }
+
+        public event EventHandler ComboCompleted;
+    }
+}
