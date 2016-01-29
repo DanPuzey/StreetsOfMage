12c5d95b3d55fa6b0450554739b7f0bb0da3b287
Dan Puzey <commit@puzey.net>
2016-01-29 23:22:03 +0000

    Add game manager and wizard player aggregate objects.

---
+++ Assets/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+
+namespace WizardDuel
+{
+    public class GameManager : MonoBehaviour
+    {
+        [Header("Game settings")]
+        public bool RandomizeSpells = false;
+        public int AlphabetSize = 4;
+        public int WinningComboSize = 10;
+
+        [Header("Component references")]
+        public WizardPlayer[] Wizards = new WizardPlayer[0];
+
+        private void Awake()
+        {
+            // wire up event handlers
+            foreach (var w in Wizards)
+            {
+                w.WinningCombo.ComboCompleted += PlayerWins;
+            }
+        }
+
+        private void Start()
+        {
+            // randomise combos
+            var winningCombo = new int[WinningComboSize];
+
+            for (var i = 0; i < WinningComboSize; i++)
+            {
+                winningCombo[i] = UnityEngine.Random.Range(0, AlphabetSize);
+            }
+
+            foreach (var w in Wizards)
+            {
+                w.WinningCombo.Combo = winningCombo;
+            }
+        }
+
+        private void PlayerWins(object sender, EventArgs e)
+        {
+            var c = sender as ComboMatcher;
+            var w = c.transform.parent.parent;
+            Debug.LogFormat("WINNER! {0}", w.name);
+        }
+
+        private void SpellCast(object sender, EventArgs e)
+        {
+            var c = sender as ComboMatcher;
+            var w = c.transform.parent.parent;
+            Debug.LogFormat("{0} casts spell", w.name);
+        }
+    }
+}
---
+++ Assets/WizardPlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace WizardDuel
+{
+    public class WizardPlayer : MonoBehaviour
+    {
+        public ComboMatcher WinningCombo;
+
+        public ComboMatcher SpeedCombo;
+        public ComboMatcher HealCombo;
+        public ComboMatcher IronBarCombo;
+        public ComboMatcher KnockBackCombo;
+        public ComboMatcher ReviveCombo;
+    }
+}
