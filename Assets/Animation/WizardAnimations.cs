2f60a3977ede04bb77f151a317de17b8b11821a3
Dan Puzey <commit@puzey.net>
2016-01-30 17:23:36 +0000

    Wire up character animations for glyph casting.

--- Assets/Animation/WizardAnimations.cs
+++ Assets/Animation/WizardAnimations.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace WizardDuel.Animation
@@ -6,8 +8,14 @@ namespace WizardDuel.Animation
     [RequireComponent(typeof(SimpleSpriteAnimator))]
     public class WizardAnimations : MonoBehaviour
     {
+        [Serializable]
+        public struct AnimFrames
+        {
+            public Sprite[] Frames;
+        }
+
         public Sprite[] IdleFrames;
-        public List<Sprite[]> GlyphCastFrames;
+        public AnimFrames[] GlyphCastFrames;
 
         private SimpleSpriteAnimator _animator;
 
@@ -18,6 +26,18 @@ namespace WizardDuel.Animation
 
         private void Start()
         {
+            _animator.Sprites = IdleFrames;
+        }
+
+        private void AddGlyph(int markIndex)
+        {
+            StartCoroutine(ShowGlyphAnim(markIndex));
+        }
+
+        private IEnumerator ShowGlyphAnim(int index)
+        {
+            _animator.Sprites = GlyphCastFrames[index].Frames;
+            yield return new WaitForSeconds(0.8f);
             _animator.Sprites = IdleFrames;
         }
     }
